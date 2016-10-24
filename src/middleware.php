ea9b67959feaaf54a8e2e59e7360f5d6e116b7f0
Dave Toews <davejtoews@gmail.com>
2016-10-24 12:04:10 -0600

    Add wildcard to Access-Control

--- src/middleware.php
+++ src/middleware.php
@@ -4,7 +4,7 @@
 $app->add(function ($req, $res, $next) {
     $response = $next($req, $res);
     return $response
-            ->withHeader('Access-Control-Allow-Origin', 'http://neo.dev')
+            ->withHeader('Access-Control-Allow-Origin', '*')
             ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
             ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
 });
